b9f954127729654690ebd11d65282131b0a482a3
serhii <ockar2018@gmail.com>
2024-09-25 14:08:53 +0300

    create requirements.txt and add functionality for admin panel

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Tag, Task
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("content", "created_at", "deadline", "is_done")
+    list_filter = ("is_done", "deadline", "tags")
+    search_fields = ("content",)
+    ordering = ("-deadline",)
+    filter_horizontal = ("tags",)
